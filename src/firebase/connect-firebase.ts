c8a6379e44da8cd74851bb9b48d7013c754f8463
Celio Almeida <acamelo13@gmail.com>
2025-06-16 09:26:54 -0300

    update env local 16/06 note to github

--- src/firebase/connect-firebase.ts
+++ src/firebase/connect-firebase.ts
@@ -1,18 +1,18 @@
+// src/firebase/connect-firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeYAYFU1sCYVE3tlETb6mXQPzuAUDjC3E",
-  authDomain: "mostracientifica2c.firebaseapp.com",
-  projectId: "mostracientifica2c",
-  storageBucket: "mostracientifica2c.firebasestorage.app",
-  messagingSenderId: "64032943083",
-  appId: "1:64032943083:web:d6f2bf94f67b8caf1547ae",
-  measurementId: "G-ST1222EW7Y"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db }
+export { db };
